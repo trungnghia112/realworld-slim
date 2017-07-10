2cb046bc8e95c21a91fbd0f448e7e28309c82ebb
Mark Railton <mark@markrailton.com>
2017-07-10 19:27:54 +0100

    Adding phpdotenv for working with environment variables

--- public/index.php
+++ public/index.php
@@ -11,6 +11,12 @@ if (PHP_SAPI == 'cli-server') {
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Check to see if we have an env file
+if (is_file(__DIR__ . '/../.env')) {
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+    $dotenv->load();
+}
+
 session_start();
 
 // Instantiate the app
